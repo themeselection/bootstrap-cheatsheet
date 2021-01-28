a62ae8efce59a98cc355ee14c8b8c1c987c83fc7
mona <monajmakwana@gmail.com>
2021-01-28 12:11:13 +0530

    (fix): added icon on hover added icons on list hover js added code to remove this code on mouseleave added next prev keyboard click js

--- src/js/cheatsheet.js
+++ src/js/cheatsheet.js
@@ -148,34 +148,78 @@ function shuffleCategory() {
   shuffleInstance.filter();
 }
 
-//---------------------- Copy ----------------------//
+// --------------------- on hover ------------------//
 
-var copyAttr = $('.list-item-copy-attr'),
-    copyCode = $('.list-item-copy-code'),
-    copySnippet = $('.copy-snippet-code');
-
-// copy to clipboard function
-function copyToClipboard(element) {
-  var $temp = $("<textarea>");
-  $("body").append($temp);
-  $temp.text(element).trigger('select');
-  document.execCommand("copy");
-  $temp.remove();
-}
-
-// copy attribute
-copyAttr.on('click', function (e) {
-  e.stopPropagation()
-  var attrVal = $(this).data('clipboard-text');
-  copyToClipboard(attrVal);
+$('.list-item').on('mouseleave', function(e){
+  $('.list-item-copy').remove();
 });
 
-// Copy Code
-copyCode.on('click', function (e) {
-  e.stopPropagation()
-  var codeSnippet = $(this).closest('.list-item').find('.code-snippet')[0].innerHTML;
-  copyToClipboard(codeSnippet);
-});
+$('.list-item').on('mouseenter', function(e){
+
+  var $this = $(this),
+  $html = "",
+  $attr = "";
+  if($($this).attr('data-clipboard-text')){
+    $attr = $($this).attr('data-clipboard-text');
+  }
+  $this.find('.list-item-copy').remove();
+  $html = `<div class="list-item-copy">`
+  if($attr !== ""){
+  $html +=  `<a class="list-item-copy-attr" data-original-title="copy to clipboard" data-clipboard-text="`+$attr+`" data-bs-toggle="tooltip" title="CSS class to clipboard">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
+        <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z"/>
+      </svg>
+    </a>`
+  }
+    $html += `<a class="list-item-copy-code" data-original-title="copy snippet to clipboard" data-bs-toggle="tooltip" title="Code snippet to clipboard">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code-slash" viewBox="0 0 16 16">
+        <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
+      </svg>
+    </a>
+  </div>`;
+
+  $this.find('.list-item-content').append($html);
+  //---------------------- Copy ----------------------//
+
+  var copyAttr = $('.list-item-copy-attr'),
+  copyCode = $('.list-item-copy-code');
+
+  // copy to clipboard function
+  function copyToClipboard(element) {
+    var $temp = $("<textarea>");
+    $("body").append($temp);
+    $temp.text(element).trigger('select');
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+  // copy attribute
+  copyAttr.on('click', function (e) {
+    e.stopPropagation()
+    var attrVal = $(this).data('clipboard-text');
+    copyToClipboard(attrVal);
+  });
+
+  // Copy Code
+  copyCode.on('click', function (e) {
+    e.stopPropagation()
+    var codeSnippet = $(this).closest('.list-item').find('.code-snippet')[0].innerHTML;
+    copyToClipboard(codeSnippet);
+  });
+
+  //---------------------- Initialize Tooltips ----------------------//
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  if (tooltipTriggerList) {
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  }
+})
+
+
+//---------------------- Copy Snippet ----------------------//
+
+var copySnippet = $('.copy-snippet-code');
 
 // Copy Code Snippet
 copySnippet.on('click', function (e) {
@@ -264,6 +308,19 @@ $('.next').on('click', function(){
   }else{
     $('.list-item.active').closest('.category').next('.shuffle-item--visible').find(".list-items .list-item:first-child").trigger("click");
     $('.list-item.active').closest('.category').find('.collapse').addClass("show")
+  }
+});
+
+$(document).on('keyup', function(e) {
+  if($('body').hasClass('modal-open')){
+    var key = e.keyCode || e.charCode || e.which;
+    switch (key) {
+    case 37:
+      $('.prev').trigger('click');
+      break;
+    case 39:
+      $('.next').trigger('click');
+    }
   }
 });
 
