a907ab48aa973632590483e00c2a1eb75f645432
mona <monajmakwana@gmail.com>
2021-01-29 16:23:41 +0530

    (fix): mixin page tooltip updated

--- src/js/cheatsheet.js
+++ src/js/cheatsheet.js
@@ -183,7 +183,7 @@ $('.list-item').on('mouseenter', function(e){
   $this.find('.list-item-copy').remove();
   if($("body").hasClass('bs-mixins')){
     $html = `<div class="list-item-copy">
-      <a class="list-item-copy-attr" data-original-title="Copy mixin name" data-bs-toggle="tooltip" title="Copy mixin name to clipboard">
+      <a class="list-item-copy-attr" data-original-title="Copy mixin name" data-bs-toggle="tooltip" title="Copy mixin name">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
           <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z"/>
         </svg>
